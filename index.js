281d89485aa0ff1a28fe821ee15ad3845183a8e5
Swoad-11 <toufiq11swoad@gmail.com>
2022-05-27 16:24:49 +0600

    mongo connected

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.udxz7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("kaathPencil").collection("products");
+
+
+    }
+}
+run().catch(console.dir);
+
+
 app.use(cors());
 app.use(express.json());
 
